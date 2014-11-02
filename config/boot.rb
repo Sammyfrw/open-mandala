70272e10d183816f353e35c69fa317926cc295bc
Mantraviz Support <support@mantraviz.com>
2014-11-02 12:47:44 -0500

    change server port to 3333. add db config for test and dev

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# change default port so we can run multiple Rails apps on one computer
+# http://stackoverflow.com/questions/3842818/how-to-change-rails-3-server-default-port-in-develoment
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port => 3333)
+    end
+  end
+end
