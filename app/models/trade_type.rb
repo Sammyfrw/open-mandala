aa6ab0c233b5c3c181b8973293c640642342704d
gruzzlymug <kevin@gruzzlymug.com>
2014-11-17 22:04:46 -0500

    add trades to app

--- app/models/trade_type.rb
+++ app/models/trade_type.rb
@@ -1,2 +1,18 @@
 class TradeType < ActiveRecord::Base
+  @@trade_types = nil
+
+  #TODO homegrown solution...what's the better way?
+  def self.all_cache
+    if @@trade_types == nil
+      @@trade_types = []
+
+      raw_types = TradeType.all.order(:group, :type_id)
+      raw_types.each do |trade_type|
+        @@trade_types.push trade_type
+      end
+    end
+
+    @@trade_types
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,22 @@ ActiveRecord::Schema.define(version: 20141126132826) do
   end
 
   create_table "trades", force: true do |t|
+    t.datetime "trade_date"
+    t.integer  "user_id"
+    t.integer  "ticker_id"
+    t.integer  "trade_type_id"
+    t.float    "num_units"
+    t.float    "price"
+    t.integer  "parent_id"
+    t.integer  "withdrawal_id"
+    t.integer  "deposit_id"
+    t.float    "commission"
+    t.float    "reg_fee"
+    t.float    "strike"
+    t.datetime "expiration"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
