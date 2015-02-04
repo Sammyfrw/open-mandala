f42fc618f09b212b22bfe121738c76fa7bbb32d7
Sammyfrw <sammyfrw@gmail.com>
2015-02-04 15:53:14 +0700

    Updated schema and gemfile

---
+++ db/migrate/20141126132826_create_trade.rb
@@ -0,0 +1,6 @@
+class CreateTrade < ActiveRecord::Migration
+  def change
+    create_table :trades do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114030713) do
+ActiveRecord::Schema.define(version: 20141126132826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20141114030713) do
     t.string   "withdrawal_description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "trades", force: true do |t|
   end
 
   create_table "users", force: true do |t|
